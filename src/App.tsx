6570430a8d26a8ba3f9a5bdc6e8eaf09977ddf85
Dima <dimalebedev6902@gmail.com>
2022-08-26 08:42:38 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lab Number 1
         </a>
       </header>
     </div>
